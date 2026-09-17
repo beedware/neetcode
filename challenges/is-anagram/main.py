fd0261ef5a4a2c05b59fb8f794787cdb913849d3
beedware <beed.epistle@outlook.com>
2026-09-17 21:31:08 +0100

    py: challenges/is-anagram

--- challenges/is-anagram/main.py
+++ challenges/is-anagram/main.py
@@ -1,20 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        pass
 
-EXAMPLES = [
-    "s=\"racecar\"\nt=\"carrace\"",
-    "s=\"jar\"\nt=\"jam\""
-]
-EXPECTED = [
-    "true",
-    "false"
-]
+        if len(s) != len(t):
+            return False
+
+        counter = {}
+        for char in s:
+            counter[char] = counter.get(char, 0) + 1
+
+        for char in t:
+            if counter.get(char, 0) == 0 or counter[char] - 1 < 0:
+                return False
+            counter[char] -= 1
+
+        return True
+
+
+EXAMPLES = ['s="racecar", t="carrace"', 's="jar", t="jam"']
+EXPECTED = ["true", "false"]
 
 
 if __name__ == "__main__":
-    from pathlib import Path
     import sys
+    from pathlib import Path
 
     sys.path.append(str(Path(__file__).resolve().parents[2] / "helper"))
     from python_runner import run_examples
