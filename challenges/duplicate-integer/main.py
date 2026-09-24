d9d7392e54b45520c0f219fdf1cbc9e0de440b3f
beedware <beed.epistle@outlook.com>
2026-09-24 01:05:52 +0100

    chore: add python type hints to challenges duplicate-integer and is-anagram

--- challenges/duplicate-integer/main.py
+++ challenges/duplicate-integer/main.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
+        seen: set = set()
         for num in nums:
             if num in seen:
                 return True
