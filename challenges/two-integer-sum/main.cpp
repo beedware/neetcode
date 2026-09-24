2cd219e0f8c51f18caf97c13ee5d493d08a0fd55
beedware <beed.epistle@outlook.com>
2026-09-24 01:31:19 +0100

    solution(cpp): challenges/two-integer-sum

--- challenges/two-integer-sum/main.cpp
+++ challenges/two-integer-sum/main.cpp
@@ -3,6 +3,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indexes;
+
+        for (int i = 0; i < static_cast<int>(nums.size()); i++) {
+            int complement = target - nums[i];
+
+            if (auto it = indexes.find(complement); it != indexes.end()) {
+                return {it->second, i};
+            }
+
+            indexes[nums[i]] = i;
+        }
+
         return {};
     }
 };
@@ -17,5 +29,5 @@ string runExample(const string& example) {
 }
 
 int main() {
-    runExamples({"nums=[3,4,5,6]\ntarget=7", "nums=[4,5,6]\ntarget=10"}, {"[0,1]", "[0,2]", "[0,1]"});
+    runExamples({"nums=[3,4,5,6]\ntarget=7", "nums=[4,5,6]\ntarget=10"}, {"[0,1]", "[0,2]"});
 }
